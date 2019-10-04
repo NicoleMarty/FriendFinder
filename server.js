477da0e65897f17d522e33926115028574ae1512
Nicole Marty <nmarty310@gmail.com>
2019-10-03 20:08:21 -0500

    creating html

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ var boyToys = [{
     name = "Jeff",
     age = 39,
     height = "about 6 feet",
-    hobbies = "picking up beautiful hoes, tinder, talking big game, getting spa treatments"
+    hobbies = "talking about bitcoin, tinder, living outside his means, getting spa treatments"
 
 }, {
     routeName = "supImDylan",
     name = "Dylan",
     age = 27,
     height = "6'2",
-    hobbies = "skateboarding, snowboarding, EDM 4 Lyfe, cocaine, handing out Red Bull on college campuses"
+    hobbies = "skateboarding, snowboarding, EDM 4 Lyfe, handing out Red Bull on college campuses"
 }, {
     routeName = "hiImGreg",
     name = "Greg",
