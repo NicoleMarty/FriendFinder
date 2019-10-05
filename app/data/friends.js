2b593e9c1cc3fbcaa3bffa7fc77840fb186f8785
Nicole Marty <nmarty310@gmail.com>
2019-10-05 17:29:15 -0500

    updating survey page to include form and a js file for materialize

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+// Data
+// ===========================================================
+var boyToys = [{
+    routeName = "myNameJeff",
+    name = "Jeff",
+    age = 39,
+    height = "about 6 feet",
+    hobbies = "talking about bitcoin, tinder, living outside his means, getting spa treatments"
+
+}, {
+    routeName = "supImDylan",
+    name = "Dylan",
+    age = 27,
+    height = "6'2",
+    hobbies = "skateboarding, snowboarding, EDM 4 Lyfe, handing out Red Bull on college campuses"
+}, {
+    routeName = "hiImGreg",
+    name = "Greg",
+    age = 34,
+    height = "I'm a tall guy",
+    hobbies = "mansplaining, drinking beer, calling everyone 'boss'"
+}];
