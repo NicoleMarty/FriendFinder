173231b131f0ea975653c1f05c0221401f6a8a57
Nicole Marty <nmarty310@gmail.com>
2019-10-06 19:42:41 -0500

    saving progres. closing down for now.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,8 @@
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+// KEEP ME - A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // gets all friends
 app.get("/api/friends", function(req, res) {
     return res.json(data);
 });
 
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+// A POST routes /api/friends. This will be used to handle incoming survey results. 
+// This route will also be used to handle the compatibility logic.
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,11 @@
-// A GET Route to /survey which should display the survey page.
+// KEEP ME - A GET Route to /survey which should display the survey page.
 app.get("/survey.html", function(req, res) {
     res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
 // A default, catch-all route that leads to home.html which displays the home page.
 
-// shows homepage text
+// KEEP ME shows homepage text
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
