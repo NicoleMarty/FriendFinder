417bc06b35b5c178aacec33364a802ca60fb6c27
Nicole Marty <nmarty310@gmail.com>
2019-10-07 12:30:13 -0500

    started readme and testing first stab at pushing user choices from survey into an array.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,3 +6,6 @@ app.get("/api/friends", function(req, res) {
 
 // A POST routes /api/friends. This will be used to handle incoming survey results. 
 // This route will also be used to handle the compatibility logic.
+app.post("/api/friends", function(req, res) {
+    return res.push(userArray);
+});
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,7 +4,6 @@ app.get("/survey.html", function(req, res) {
 });
 
 // A default, catch-all route that leads to home.html which displays the home page.
-
 // KEEP ME shows homepage text
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "app/public/home.html"));
