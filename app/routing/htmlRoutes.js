cff11ca7a80eb27cbab40b2d3a3e57787fc79336
Nicole Marty <nmarty310@gmail.com>
2019-10-07 16:51:52 -0500

    still getting a runtime error, but things seem to be working as far as flipping between home and survey pages...

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,14 +2,14 @@ var path = require("path");
 
 module.exports = function(app) {
 
-    // KEEP ME - A GET Route to /survey which should display the survey page.
-    app.get("/survey.html", function(req, res) {
-        res.sendFile(path.join(__dirname, "app/public/survey.html"));
-    });
-
     // A default, catch-all route that leads to home.html which displays the home page.
     // KEEP ME shows homepage text
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "app/public/home.html"));
+    });
+
+    // KEEP ME - A GET Route to /survey which should display the survey page.
+    app.get("/survey.html", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 };
