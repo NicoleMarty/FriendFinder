a054eb83b7c80790e08a74e36a83b61c47c68110
Nicole Marty <nmarty310@gmail.com>
2019-10-08 11:26:30 -0500

    updated survey.html to include name and image again. got deleted somehow.

--- app/data/user.js
+++ app/data/user.js
@@ -8,6 +8,10 @@ var PORT = 3000;
 
 // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
 // ---------------------------------------------------------------------------------------------------
+
+
+// compare the difference between current user's scores against those from other users, question by question.
+// ---------------------------------------------------------------------------------------------------
 var userArray = [{
     "name": "Nicole",
     "photo": "",
@@ -27,9 +31,6 @@ var userArray = [{
 
 module.exports = userArray;
 
-// compare the difference between current user's scores against those from other users, question by question.
-// ---------------------------------------------------------------------------------------------------
-
 // Add up the differences to calculate the totalDifference.
 // Remember to use the absolute value of the differences.
 // Your app should calculate both 5-3 and 3-5 as 2
@@ -38,37 +39,6 @@ module.exports = userArray;
 
 // Once you've found the current user's most compatible friend, display the result as a modal pop-up.
 // The modal should display both the name and picture of the closest match.
-// KEEP ME shows homepage text
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-
-// KEEP ME gets all friendssurvey which should display the survey page.
-app.get("/survey.html", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
-
-// KEEP ME gets all friends
-app.get("/api/user", function(req, res, userQuery) {
-    return res.json(userArray);
-
-    function userQuery() {
-        $.ajax({
-                url: "/api/user",
-                method: "GET"
-            })
-            .then(function(userArray) {
-
-                // Here we then log the tableData to console, where it will show up as an object.
-                console.log(userArray);
-                console.log("------------------------------------");
-
-            });
-    }
-});
-
-
-
 
 
 // Listener
