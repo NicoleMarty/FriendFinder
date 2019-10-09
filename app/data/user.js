50028b1da0b58f4ea182737d62eabbcdadf5e600
Nicole Marty <nmarty310@gmail.com>
2019-10-09 13:50:05 -0500

    tried to get a modal to pop up just with array[0]. no good

--- app/data/user.js
+++ app/data/user.js
@@ -4,7 +4,10 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
 // ---------------------------------------------------------------------------------------------------
